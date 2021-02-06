ce49ace92f3ff64b4a8da10eee1db024553b6390
duyvu871 <78553549+duyvu871@users.noreply.github.com>
2021-02-06 22:34:46 +0700

    add some code

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ console.log('hello')
 // undefined 
 // null
 // NaN
+
+//gửi request
+function git(request) { console.log(JSON.stringify(request)); }
+const inF = [
+    { name:'du', age:16 ,height:23, },
+    { name:'thai', age:34 ,height:45, },  
+    { name:'hung', age:17 ,height:56, },
+    { name:'repull', age:15 ,height:70, },
+    { name:'under', age:47 ,height:14, },
+    { name:'hung', age:17 ,height:56, },
+];
+const getName = inF.reduce((element,value) => element.concat(value.name) ,[]);
+const getValueInput = document.querySelector('#getInf');
+getValueInput.onblur = (e)=>{
+    if (getName.includes(e.target.value,0)) {
+        const find = inF.filter(element=> element.name === e.target.value);
+        find.forEach((element,index)=> { git({...element,id:index}) });
+    }else git(undefined);
+}
